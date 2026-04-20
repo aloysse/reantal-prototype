442168421dd3f8f79bd2e0ab4c8c7444610bd4c1
Macbook Pro <macbookpro@houseflow.tw>
2026-04-20 10:59:39 +0800

    feat: 將mui space 單位改成 4px

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 import './styles/index.css'
 import App from './app/App.tsx'
 
+const theme = createTheme({
+  spacing: 4,
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
